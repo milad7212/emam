20349891730b4e43725b5e6afc206de32b353f23
Milad Hasani <milad7212@gmail.com>
2023-12-27 18:44:54 +0330

    add font

--- app/components/forms/Stacked.jsx
+++ app/components/forms/Stacked.jsx
@@ -618,40 +618,7 @@ export default function Stacked() {
                 </div>
               </div>
             </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="cover-photo"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                شناسنامه
-              </label>
-              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                <div className="text-center">
-                  {/* <PhotoIcon
-                    className="mx-auto h-12 w-12 text-gray-300"
-                    aria-hidden="true"
-                  /> */}
-                  <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                    <label
-                      htmlFor="file-upload"
-                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                    >
-                      <span className="p-3"> آپلود عکس</span>
-                      <input
-                        id="file-upload"
-                        name="file-upload"
-                        type="file"
-                        className="sr-only"
-                      />
-                    </label>
-                    <p className="pl-1"> یا فایل را بکشید </p>
-                  </div>
-                  <p className="text-xs leading-5 text-gray-600">
-                    PNG, JPG, GIF up to 10MB
-                  </p>
-                </div>
-              </div>
-            </div>
+
             <div className="sm:col-span-2">
               <label
                 htmlFor="cover-photo"
